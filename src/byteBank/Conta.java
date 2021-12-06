1ddb19da2ae4e581bd2bba74820bc0e64351dbe3
Mateus Souza <mateus_souza11@hotmail.com.br>
2021-12-06 17:39:42 -0300

    começando o projeto

---
+++ src/byteBank/Conta.java
@@ -0,0 +1,8 @@
+package byteBank;
+
+public class Conta {
+	public int conta;
+	public double saldo =100;
+	public int agencia;
+	public int numero;	
+}
---
+++ src/byteBank/main.java
@@ -0,0 +1,8 @@
+package byteBank;
+
+public class main {
+	public static void main(String[] args) {
+		Conta primeira = new Conta();
+		System.out.println(primeira.saldo);;
+	}
+}
